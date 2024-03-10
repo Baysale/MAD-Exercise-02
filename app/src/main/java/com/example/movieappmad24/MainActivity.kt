1695dd44c0b01ea3ea9b1858a378d59ea259bd03
Enes Baysal <enesbaysal_68@hotmail.com>
2024-03-10 15:28:57 +0100

    - add bar - add animatedvisbility - add coil asyncimage

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.movieappmad24
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,18 +19,23 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ShapeDefaults
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,30 +44,75 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierInfo
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.movieappmad24.models.BottomItem
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppMAD24Theme {
-                // A surface container using the 'background' color from the theme
+                val items = listOf(
+                    BottomItem(
+                        title = "Home",
+                        icon = Icons.Filled.Home,
+                    ),
+                    BottomItem(
+                        title = "Watchlist",
+                        icon = Icons.Filled.Star,
+                    )
+                )
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MovieList(movies = getMovies())
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        topBar = {
+                            TopAppBar(title = {
+                                Text(text = "Movie App",
+                                    modifier = Modifier.fillMaxWidth(),
+                                    textAlign = TextAlign.Center) })
+                        },
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed{i, item ->
+                                    NavigationBarItem(
+                                        onClick = {},
+                                        selected = false,
+                                        label = {
+                                            Text(text = item.title)
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = item.icon,
+                                                contentDescription = item.title
+                                            )
+                                        })
+                                }
+                            }
+                        }
+                    ) {
+                        Box(modifier = Modifier
+                            .padding(top = 80.dp, bottom = 80.dp)) {
+                            MovieList()
+                        }
+                    }
+
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun MovieList(movies: List<Movie> = getMovies()){
@@ -91,10 +143,11 @@ fun MovieRow(movie: Movie){
                     .fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.movie_image),
+                AsyncImage(
+                    model = movie.images.first(),
                     contentScale = ContentScale.Crop,
-                    contentDescription = "placeholder image")
+                    contentDescription = "placeholder image"
+                )
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
@@ -107,7 +160,7 @@ fun MovieRow(movie: Movie){
                         contentDescription = "Add to favorites")
                 }
             }
-            
+
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -118,20 +171,33 @@ fun MovieRow(movie: Movie){
                 Text(text = movie.title)
                 Icon(modifier = Modifier
                     .clickable {
-                       showDetails = !showDetails
+                        showDetails = !showDetails
                     },
                     imageVector =
-                        if (showDetails) Icons.Filled.KeyboardArrowDown
-                        else Icons.Default.KeyboardArrowUp, contentDescription = "show more")
+                    if (showDetails) Icons.Filled.KeyboardArrowDown
+                    else Icons.Default.KeyboardArrowUp, contentDescription = "show more"
+                )
+            }
+        }
+        AnimatedVisibility (showDetails) {
+            Column {
+                Text(text = "Director: ${movie.director}")
+                Text(text = "Released: ${movie.year}")
+                Text(text = "Genre: ${movie.genre}")
+                Text(text = "Actors: ${movie.actors}")
+                Text(text = "Rating: ${movie.rating}")
+                Divider()
+                Text(text = "Plot: ${movie.plot}")
             }
         }
     }
 }
 
+
 @Preview
 @Composable
 fun DefaultPreview(){
     MovieAppMAD24Theme {
-       MovieList(movies = getMovies())
+        MovieList(movies = getMovies())
     }
 }
---
+++ app/src/main/java/com/example/movieappmad24/models/BottomItem.kt
@@ -0,0 +1,8 @@
+package com.example.movieappmad24.models
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomItem(
+    val title: String,
+    val icon: ImageVector,
+)
